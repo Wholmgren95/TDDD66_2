28e80f0dbc04e0dbbebd51dd291d28cd547e0a7c
Wille <wholmgren95@hotmail.com>
2018-09-27 11:11:11 +0200

    throughput done

--- src/Simulator.java
+++ src/Simulator.java
@@ -1,5 +1,18 @@
-/**
- * Created by wholm on 2018-09-26.
- */
+import java.io.*;
+import java.util.*;
+
 public class Simulator {
+    public static void main(String args[]) throws IOException {
+        //log into throughput, tp
+        ArrayDeque tp = new ArrayDeque();
+        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Wille\\IdeaProjects\\TDDD66_2\\src\\dl.log"));
+        String line;
+        while ((line = reader.readLine()) != null){
+            String[] parts = line.split("\\s+");
+            tp.add(Integer.parseInt(parts[4]));
+        }
+
+
+    }
 }
+
